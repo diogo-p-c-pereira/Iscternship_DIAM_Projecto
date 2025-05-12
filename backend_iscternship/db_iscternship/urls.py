6c84508ee68c0c945b9d2a05473ba8336acd29aa
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-05-13 00:48:04 +0100

    Some Views done

--- backend_iscternship/db_iscternship/urls.py
+++ backend_iscternship/db_iscternship/urls.py
@@ -4,4 +4,7 @@ app_name ='db_iscternship'
 
 urlpatterns = [
  path('api/candidato/<int:candidato_id>', views.candidato),
+ path("api/signup/", views.signup),
+ path("api/login/", views.login_view),
+ path("api/logout/", views.logout_view),
 ]
--- backend_iscternship/db_iscternship/views.py
+++ backend_iscternship/db_iscternship/views.py
@@ -6,6 +6,8 @@ from .serializers import *
 from .models import *
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -18,10 +20,45 @@ def candidato(request, candidato_id):
         serializer = CandidatoSerializer(candidato)
         return Response(serializer.data)
 
-    elif request.method == 'POST':  # (3)
-        serializer = CandidatoSerializer(data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
+    #elif request.method == 'POST':  # (3)
+     #   serializer = CandidatoSerializer(data=request.data)
+#
+ #       if serializer.is_valid():
+  #          serializer.save()
 
     return Response(status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email')
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+
+    if username is None or password is None:
+        return Response({'error': 'invalid username/password'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+    candidato = Candidato(user = user, descricao = request.data.get('descricao'))
+    candidato.save()
+    return Response({'message': 'User ' + user.username + ' created successfully'}, status=status.HTTP_201_CREATED)
+
+@api_view(['POST'])
+def login_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user) # Criação da sessão
+        return Response({'message': 'Logged in successfully'})
+    else:
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['GET'])
+def logout_view(request):
+    logout(request)
+    return Response({'message': 'Logged out successfully'})
