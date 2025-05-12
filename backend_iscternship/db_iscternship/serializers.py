7a9008e8a7513182fd111881fb7b9b6b4a3fa52a
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-05-13 00:27:06 +0100

    Serializers created, not tested yet

--- backend_iscternship/db_iscternship/serializers.py
+++ backend_iscternship/db_iscternship/serializers.py
@@ -1,17 +1,42 @@
 from rest_framework import serializers
 from .models import *
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
 class CandidatoSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Candidato
-        fields = ('user.username', 'user.first_name', 'user.last_name', 'user.email', 'descricao')
+        fields = '__all__'
 
 class EmpresaSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Empresa
-        fields = ('user.username', 'user.first_name', 'user.last_name', 'user.email', 'nome_empresa', 'morada')
+        fields = '__all__'
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ('candidato.user', 'empresa.user', 'commentario', 'data')
+        fields = '__all__'
+        
+        
+class VagaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vaga
+        fields = '__all__'
+
+class CandidaturaSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Candidatura
+        fields = '__all__'
+
+class ReportßSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = '__all__'
+
--- backend_iscternship/db_iscternship/views.py
+++ backend_iscternship/db_iscternship/views.py
@@ -4,11 +4,14 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from .serializers import *
 from .models import *
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
 @api_view(['GET', 'POST'])  # (2)
+@permission_classes([IsAuthenticated])
 def candidato(request, candidato_id):
     if request.method == 'GET':  # (3)
         candidato = Candidato.objects.get(pk=candidato_id)
@@ -17,6 +20,7 @@ def candidato(request, candidato_id):
 
     elif request.method == 'POST':  # (3)
         serializer = CandidatoSerializer(data=request.data)
+        
         if serializer.is_valid():
             serializer.save()
 
