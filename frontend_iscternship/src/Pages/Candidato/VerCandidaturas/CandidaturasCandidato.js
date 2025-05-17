167bdebee48b2ce3028876290486d12fa3695d30
Diogo Pereira <113448154+diogo-p-c-pereira@users.noreply.github.com>
2025-05-17 13:15:14 +0100

    Divided CandituraDetalhes into component

--- frontend_iscternship/src/Pages/Candidato/VerCandidaturas/CandidaturasCandidato.js
+++ frontend_iscternship/src/Pages/Candidato/VerCandidaturas/CandidaturasCandidato.js
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import { Button } from "reactstrap";
 import VagaDetalhes from '../../../Components/VagaDetalhes';
 import {useUserContext} from "../../../UserProvider";
+import CandidaturaDetalhes from "../../../Components/CandidaturaDetalhes";
 
 
 function CandidaturasCandidato() {
@@ -84,42 +85,7 @@ function CandidaturasCandidato() {
         {cDetalhe && (
             <div className="vagas-modal-bg" onClick={() => setCDetalhe(null)}>
               <div className="vagas-modal-form" onClick={e => e.stopPropagation()}>
-                <h2>Candidatura: {cDetalhe.vaga.titulo}</h2>
-                {console.log(cDetalhe)}
-                <div><strong>Estado:</strong> {cDetalhe.estado}</div>
-                <div><strong>Data envio:</strong> {cDetalhe.data_envio}</div>
-                <div><strong>Empresa:</strong> {cDetalhe.vaga.empresa.nome_empresa}</div>
-                <br/>
-                <div><strong>CV Enviado: </strong>
-                  {((cDetalhe.cv && !cDetalhe.cv.includes('empty.pdf'))
-        ? (cDetalhe.cv.startsWith('http')
-            ? cDetalhe.cv
-            : `http://localhost:8000/${cDetalhe.cv}`)
-        : null) ? (
-                    <a
-                      href={`http://localhost:8000/${cDetalhe.cv}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{
-                        color: "#a3b6d9",
-                        display: "block",
-                        marginBottom: 6,
-                        fontWeight: "bold"
-                      }}
-                    >
-                      {cDetalhe.cv.split('/').pop()}
-                    </a>
-                  ) : (
-                    <span style={{ color: "#a3b6d9", fontSize: '0.95rem', marginBottom: 6 }}>
-                      Nenhum CV enviado
-                    </span>
-                  )}</div>
-                <div className="vaga-descricao-detalhe">
-                  <strong>Feedback:</strong>
-                  <div className="vaga-descricao-box">
-                    {cDetalhe.feedback_empresa? cDetalhe.feedback_empresa: "Feedback não disponivel"}
-                  </div>
-                </div>
+                <CandidaturaDetalhes cDetalhe={cDetalhe} />
                 <button
                     type="button"
                     className="register-button vagas-modal-fechar"
